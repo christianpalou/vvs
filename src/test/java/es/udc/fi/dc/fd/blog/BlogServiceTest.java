46fe824fbeb2726ba54afc03bd2734fd0ba33248
christian.palou <christian.palou@udc.es>
2016-10-20 15:57:14 +0200

    BlogServiceTest

--- src/test/java/es/udc/fi/dc/fd/blog/BlogServiceTest.java
+++ src/test/java/es/udc/fi/dc/fd/blog/BlogServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,26 +35,37 @@ public class BlogServiceTest {
 
 	@Autowired
 	private BlogRepository blogRepository;
+	
+	Account account;
+	Account account2;
+	Account account3;
 
+	@Before
+	public void initialice() throws es.udc.fi.dc.fd.account.SaveNotAvailableException{
+		account = accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
+		account2 = accountService.save(new Account("brais@udc.es", "brais", "ROLE_USER", "brais"));
+		account3 = accountService.save(new Account("christian@udc.es", "christian", "ROLE_USER", "christian"));
+	}
+	
+	//PR-UN-001
 	@Test
 	public void testSaveBlog() throws SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		Blog foundBlog=blogRepository.findOneByName(blog.getName());
 		assertEquals(blog, foundBlog);
 	}
 
+	//PR-UN-002
 	@Test(expected = SaveNotAvailableException.class)
 	public void testSaveBlogWithExistingName() throws SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		blogService.save(new Blog("MyBlog", "BraisBlog", "This is the blog of Brais", false, account));
 
 	}
 
+	//PR-UN-003
 	@Test
 	public void testUpdateBlog() throws SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		blog.setName("YourBlog");
 		blog.setTitle("BraisBlog");
@@ -65,11 +78,9 @@ public class BlogServiceTest {
 		assertEquals(updatedBlog, foundBlog);
 	}
 
+	//PR-UN-004
 	@Test
 	public void testFollow() throws FollowException, InstanceNotFoundException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
-		Account account2=accountService.save(new Account("brais@udc.es", "brais", "ROLE_USER", "brais"));
-		Account account3=accountService.save(new Account("christian@udc.es", "christian", "ROLE_USER", "christian"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		Blog blog2 =blogService.save(new Blog("YourBlog", "AdriBlog2", "This is the second blog of Adri", true, account));
 		blogService.follow(account2.getId(), blog.getBlogId());
@@ -88,10 +99,9 @@ public class BlogServiceTest {
 		assertTrue(expectedRequests.equals(blog2.getFollowRequests()));
 	}
 
+	//PR-UN-005
 	@Test
 	public void testFollowNotAvailable() throws InstanceNotFoundException, FollowException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
-		Account account2=accountService.save(new Account("brais@udc.es", "brais", "ROLE_USER", "brais"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		Blog blog2 =blogService.save(new Blog("YourBlog", "AdriBlog2", "This is the second blog of Adri", true, account));
 
@@ -125,24 +135,22 @@ public class BlogServiceTest {
 		assertTrue(exceptionCatched);
 	}
 
+	//PR-UN-006
 	@Test(expected = InstanceNotFoundException.class)
 	public void testFollowNoExistentBlog() throws FollowException, InstanceNotFoundException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		blogService.follow(account.getId(), NON_EXISTENT_ID);
 	}
 
+	//PR-UN-007
 	@Test(expected = InstanceNotFoundException.class)
 	public void testFollowWithNoExistentAccount() throws FollowException, InstanceNotFoundException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		blogService.follow(NON_EXISTENT_ID, blog.getBlogId());
 	}
 
+	//PR-UN-008
 	@Test
 	public void testUnfollow() throws FollowException, InstanceNotFoundException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
-		Account account2=accountService.save(new Account("brais@udc.es", "brais", "ROLE_USER", "brais"));
-		Account account3=accountService.save(new Account("christian@udc.es", "christian", "ROLE_USER", "christian"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		blogService.follow(account2.getId(), blog.getBlogId());
 		blogService.follow(account3.getId(), blog.getBlogId());
@@ -162,10 +170,9 @@ public class BlogServiceTest {
 		assertTrue(expectedBlogs.equals(account3.getFollowingBlogs()));
 	}
 
+	//PR-UN-009
 	@Test
 	public void testUnfollowNotAvailable() throws InstanceNotFoundException, FollowException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
-		Account account2=accountService.save(new Account("brais@udc.es", "brais", "ROLE_USER", "brais"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 
 		//Prueba dejar de seguir a blog no seguido anteriormente
@@ -178,23 +185,22 @@ public class BlogServiceTest {
 		assertTrue(exceptionCatched);
 	}
 
+	//PR-UN-010
 	@Test(expected = InstanceNotFoundException.class)
 	public void testUnfollowNoExistentBlog() throws FollowException, InstanceNotFoundException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		blogService.unfollow(account.getId(), NON_EXISTENT_ID);
 	}
 
+	//PR-UN-011
 	@Test(expected = InstanceNotFoundException.class)
 	public void testUnfollowWithNoExistentAccount() throws FollowException, InstanceNotFoundException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		blogService.unfollow(NON_EXISTENT_ID, blog.getBlogId());
 	}
 
+	//PR-UN-012
 	@Test
 	public void testAcceptFollowRequest() throws FollowException, InstanceNotFoundException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
-		Account account2=accountService.save(new Account("brais@udc.es", "brais", "ROLE_USER", "brais"));
 		Blog blog2 =blogService.save(new Blog("YourBlog", "AdriBlog2", "This is the second blog of Adri", true, account));
 		blogService.follow(account2.getId(), blog2.getBlogId());
 		blogService.acceptFollowRequest(account2.getId(), blog2.getBlogId());
@@ -207,10 +213,9 @@ public class BlogServiceTest {
 		assertTrue(blog2.getFollowRequests().isEmpty());
 	}
 
+	//PR-UN-013
 	@Test
 	public void testAcceptFollowRequestNotAvailable() throws InstanceNotFoundException, FollowException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
-		Account account2=accountService.save(new Account("brais@udc.es", "brais", "ROLE_USER", "brais"));
 		Blog blog2 =blogService.save(new Blog("YourBlog", "AdriBlog2", "This is the second blog of Adri", true, account));
 
 		//Prueba aceptar peticion de cuenta seguidora
@@ -244,23 +249,22 @@ public class BlogServiceTest {
 		assertTrue(exceptionCatched);
 	}
 
+	//PR-UN-014
 	@Test(expected = InstanceNotFoundException.class)
 	public void testAcceptFollowRequestNoExistentBlog() throws FollowException, InstanceNotFoundException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		blogService.acceptFollowRequest(account.getId(), NON_EXISTENT_ID);
 	}
 
+	//PR-UN-015
 	@Test(expected = InstanceNotFoundException.class)
 	public void testAcceptFollowRequestWithNoExistentAccount() throws FollowException, InstanceNotFoundException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		Blog blog2 =blogService.save(new Blog("YourBlog", "AdriBlog2", "This is the second blog of Adri", true, account));
 		blogService.acceptFollowRequest(NON_EXISTENT_ID, blog2.getBlogId());
 	}
 
+	//PR-UN-016
 	@Test
 	public void testDenyFollowRequest() throws FollowException, InstanceNotFoundException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
-		Account account2=accountService.save(new Account("brais@udc.es", "brais", "ROLE_USER", "brais"));
 		Blog blog2 =blogService.save(new Blog("YourBlog", "AdriBlog2", "This is the second blog of Adri", true, account));
 		blogService.follow(account2.getId(), blog2.getBlogId());
 		blogService.denyFollowRequest(account2.getId(), blog2.getBlogId());
@@ -269,10 +273,9 @@ public class BlogServiceTest {
 		assertTrue(blog2.getFollowRequests().isEmpty());
 	}
 
+	//PR-UN-017
 	@Test
 	public void testDenyFollowRequestNotAvailable() throws InstanceNotFoundException, FollowException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
-		Account account2=accountService.save(new Account("brais@udc.es", "brais", "ROLE_USER", "brais"));
 		Blog blog2 =blogService.save(new Blog("YourBlog", "AdriBlog2", "This is the second blog of Adri", true, account));
 
 		//Prueba denegar peticion de cuenta seguidora
@@ -306,30 +309,30 @@ public class BlogServiceTest {
 		assertTrue(exceptionCatched);
 	}
 
+	//PR-UN-018
 	@Test(expected = InstanceNotFoundException.class)
 	public void testDenyFollowRequestNoExistentBlog() throws FollowException, InstanceNotFoundException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		blogService.denyFollowRequest(account.getId(), NON_EXISTENT_ID);
 	}
 
+	//PR-UN-019
 	@Test(expected = InstanceNotFoundException.class)
 	public void testDenyFollowRequestWithNoExistentAccount() throws FollowException, InstanceNotFoundException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		Blog blog2 =blogService.save(new Blog("YourBlog", "AdriBlog2", "This is the second blog of Adri", true, account));
 		blogService.denyFollowRequest(NON_EXISTENT_ID, blog2.getBlogId());
 	}
 
+	//PR-UN-020
 	@Test
 	public void testFindBlogByName() throws SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		Blog foundBlog= blogService.findBlogByName(blog.getName());
 		assertEquals(blog,foundBlog);
 	}
 
+	//PR-UN-021
 	@Test
 	public void testFindBlogs() throws SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		Blog blog2 =blogService.save(new Blog("YourBlog", "AdriBlog2", "This is the second blog of Adri", true, account));
 		List<Blog> foundBlogs= blogService.findBlogs("zzz");
@@ -343,47 +346,52 @@ public class BlogServiceTest {
 		assertEquals(expectedBlogs,foundBlogs);	
 	}
 
+	//PR-UN-022
 	@Test
 	public void testIsFollower() throws InstanceNotFoundException, FollowException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
-		Account account2=accountService.save(new Account("brais@udc.es", "brais", "ROLE_USER", "brais"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		blogService.follow(account2.getId(), blog.getBlogId());
 		assertTrue(blogService.isFollower(account2.getId(), blog.getBlogId()));
 	}
 
+	//PR-UN-023
 	@Test(expected = InstanceNotFoundException.class)
 	public void testIsFollowerNoExistentBlog() throws FollowException, InstanceNotFoundException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		blogService.isFollower(account.getId(), NON_EXISTENT_ID);
 	}
 
+	//PR-UN-024
 	@Test(expected = InstanceNotFoundException.class)
 	public void testIsFollowerWithNoExistentAccount() throws FollowException, InstanceNotFoundException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		Blog blog =blogService.save(new Blog("MyBlog", "AdriBlog", "This is the blog of Adri", false, account));
 		blogService.isFollower(NON_EXISTENT_ID, blog.getBlogId());
 	}
 
+	//PR-UN-025
 	@Test
 	public void testIsFollowRequest() throws InstanceNotFoundException, FollowException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
-		Account account2=accountService.save(new Account("brais@udc.es", "brais", "ROLE_USER", "brais"));
 		Blog blog2 =blogService.save(new Blog("YourBlog", "AdriBlog2", "This is the second blog of Adri", true, account));
 		blogService.follow(account2.getId(), blog2.getBlogId());
 		assertTrue(blogService.isFollowRequest(account2.getId(), blog2.getBlogId()));
 	}
 
+	//PR-UN-026
 	@Test(expected = InstanceNotFoundException.class)
 	public void testIsFollowRequestNoExistentBlog() throws FollowException, InstanceNotFoundException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		blogService.isFollowRequest(account.getId(), NON_EXISTENT_ID);
 	}
 
+	//PR-UN-027
 	@Test(expected = InstanceNotFoundException.class)
 	public void testIsFollowRequestWithNoExistentAccount() throws FollowException, InstanceNotFoundException, SaveNotAvailableException, es.udc.fi.dc.fd.account.SaveNotAvailableException{
-		Account account=accountService.save(new Account("adri@udc.es", "adri", "ROLE_USER", "adri"));
 		Blog blog2 =blogService.save(new Blog("YourBlog", "AdriBlog2", "This is the second blog of Adri", true, account));
 		blogService.isFollowRequest(NON_EXISTENT_ID, blog2.getBlogId());
+	}
+	
+	@After
+	public void endTest() throws es.udc.fi.dc.fd.account.SaveNotAvailableException{
+		account = null;
+		account2 = null;
+		account3 = null;
 	}
 }
