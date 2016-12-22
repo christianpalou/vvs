aa2a84fff486c744b4ac488b4d2c6661a23d26f7
christian.palou <christian.palou@udc.es>
2016-12-22 17:14:26 +0100

    Cambiando tabs por espacios

--- src/test/java/es/udc/fi/dc/fd/account/AccountGenerator.java
+++ src/test/java/es/udc/fi/dc/fd/account/AccountGenerator.java
@@ -6,16 +6,15 @@ import net.java.quickcheck.generator.PrimitiveGenerators;
 
 public class AccountGenerator implements Generator<Account> {
 
-  
   Generator<String> mail = PrimitiveGenerators.strings("abcdefghijklmnñopqrstuvwxyz", 1, 6);
   Generator<String> user = PrimitiveGenerators.strings("abcdefghijklmnñopqrstuvwxyz", 1, 6);
   Generator<String> pass = PrimitiveGenerators.strings("abcdefghijklmnñopqrstuvwxyz", 1, 6);
-  
+
   @Override
   public Account next() {
-    
-      return new Account(mail.next()+"@udc.es", pass.next(), "ROLE_USER", user.next());
 
-   }
-  
+    return new Account(mail.next() + "@udc.es", pass.next(), "ROLE_USER", user.next());
+
+  }
+
 }
